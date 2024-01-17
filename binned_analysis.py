0e67dd384d4d3397033570cf7d6d445d03c349ea
Philip <philip@northarrowresearch.com>
2024-01-17 10:47:49 -0800

    fixing raster imports in binned and incremental

--- binned_analysis.py
+++ binned_analysis.py
@@ -4,7 +4,7 @@ Run the binned analysis
 import csv
 from typing import Dict, List
 from raster_analysis import get_vol_and_area
-from raster import Raster
+from Raster import Raster
 from logger import Logger
 from sandbar_site import SandbarSite
 from analysis_bin import AnalysisBin
